86bf996f14acde7d6eeecb64ab64537a0fc9f6df
pigghead <justin.strongarms10@gmail.com>
2021-03-03 13:04:15 -0500

    Added jumping. Prevented double jumping and wall jumping. Bug: character can stick to walls

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private bool IsTouchingGround = false;
 
-    public Vector2 MovementSpeed;
-    public float MaxSpeed;
+    public float MovementSpeed = 100.0f;
+    public float MaxSpeed = 16.0f;
+    public float JumpForce = 6.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
-        MovementSpeed = new Vector2(100.0f, 0.0f);
-        MaxSpeed = 8.0f;
     }
 
     // Update is called once per frame
@@ -27,14 +27,16 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Movement();
+        Jump();
     }
 
-    void Movement() {
+    void Movement()
+    {
         // GetAxisRaw checks for WASD and arrow key input; horizontal refers to AD/Left and Right
         if(Input.GetAxisRaw("Horizontal") == 1)  // 1 is positive x direction
         {
             // Adding force to our rigid body in the positive x direction
-            rb.AddForce(MovementSpeed);
+            rb.AddForce(new Vector2(MovementSpeed, 0));
             // Clamp our movement speed to MaxSpeed variable
             if (rb.velocity.x > MaxSpeed)
             {
@@ -44,7 +46,7 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetAxisRaw("Horizontal") == -1)  // negative x direction
         {
             // Force in the negative x direction
-            rb.AddForce(-MovementSpeed);
+            rb.AddForce(new Vector2(-MovementSpeed, 0));
             // clamping again
             if (rb.velocity.x < -MaxSpeed)
             {
@@ -56,5 +58,34 @@ public class PlayerController : MonoBehaviour
             // immediately set our x velocity to zero when releasing movement key
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
+    }
+
+    void Jump()
+    {
+        // If we're touching the ground and the jump key is pressed
+        if(Input.GetKeyDown(KeyCode.Space) && IsTouchingGround)
+        {
+            // Add a jump force, set touching ground to false
+            rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            IsTouchingGround = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Get a vector from the player to the object we have just touched
+        Vector2 Direction = (collision.gameObject.transform.position - this.gameObject.transform.position);
+
+        // If we have touched the ground, reset our jump
+        if (Direction.y < 0)
+        {
+            IsTouchingGround = true;
+        }
+
+        // Useful for detecting top collisions in the future
+        //if (Direction.y > 0)
+        //{
+        //    Debug.Log("top");
+        //}
     }
 }
